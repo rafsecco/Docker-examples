5d4f58dd7b42fed7a666eb6b0d8efabec0cece31
Rafael Secco <raf.secco@gmail.com>
2026-08-07 08:32:32 -0300

    Remove deprecated WordPress Bedrock configuration files and local development setup

--- WordPress/Readme.md
+++
@@ -1,2 +0,0 @@
-
-Imagens mais seguras e estáveis: Considere a imagem do Bitnami (bitnami/wordpress), que já é configurada com segurança aprimorada.
--- WordPress/local-dev/nginx/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Some logging
-echo
-echo 'nginx is up and running. You should be able to access'
-echo "it under: http://${HOSTNAME} or https://${HOSTNAME}"
-
-# Start nginx like normal
-nginx -g 'daemon off;'
